5b8ad29d1ada482b6a481118bcd5be7ce3faf1fa
shenmiweiyi <shenmiweiyi@163.com>
2015-05-13 11:59:25 +0800

    fix first click select item appear flickr sometimes

--- js/jquery.selectlist.js
+++ js/jquery.selectlist.js
@@ -376,7 +376,7 @@
                     .children('.select-button').removeAttr('disabled');
                     
                 //绑定单击选项事件
-                $(selectID + ' li').mousedown(function(event){
+                $(selectID + ' li').on('click',function(event){
                     event.stopPropagation();                    
                     $(this)
                         .addClass('selected').siblings().removeClass('selected')
